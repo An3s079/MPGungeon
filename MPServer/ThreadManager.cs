4431a99d58c2cd5d3bf6b2e53135c7252422d332
An3s079 <An3sFerreira@gmail.com>
2021-03-26 22:08:53 -1000

    copied a lot of HKMP code and edited some code/added comments

---
+++ MPServer/ThreadManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MPGungeon.MPServer
+{
+    //already commented.
+    class ThreadManager : MonoBehaviour
+    {
+        private static readonly List<Action> ToExecuteOnMainThread = new List<Action>();
+        private static readonly List<Action> ExecuteCopiedOnMainThread = new List<Action>();
+        private static bool _actionToExecuteOnMainThread;
+
+        private void FixedUpdate()
+        {
+            UpdateMain();
+        }
+
+        /// <summary>Sets an action to be executed on the main thread.</summary>
+        /// <param name="action">The action to be executed on the main thread.</param>
+        public static void ExecuteOnMainThread(Action action)
+        {
+            if (action == null)
+            {
+                Log("No action to execute on main thread!");
+                return;
+            }
+
+            lock (ToExecuteOnMainThread)
+            {
+                ToExecuteOnMainThread.Add(action);
+                _actionToExecuteOnMainThread = true;
+            }
+        }
+
+        /// <summary>Executes all code meant to run on the main thread. NOTE: Call this ONLY from the main thread.</summary>
+        public static void UpdateMain()
+        {
+            if (_actionToExecuteOnMainThread)
+            {
+                ExecuteCopiedOnMainThread.Clear();
+                lock (ToExecuteOnMainThread)
+                {
+                    ExecuteCopiedOnMainThread.AddRange(ToExecuteOnMainThread);
+                    ToExecuteOnMainThread.Clear();
+                    _actionToExecuteOnMainThread = false;
+                }
+
+                for (int i = 0; i < ExecuteCopiedOnMainThread.Count; i++)
+                {
+                    ExecuteCopiedOnMainThread[i]();
+                }
+            }
+        }
+
+        private static void Log(object message) => Debug.Log("[Thread Manager] " + message);
+    }
+}
--- Module.cs
+++ Module.cs
@@ -7,77 +7,34 @@ using System.Text;
 using Gungeon;
 using UnityEngine;
 using System.Globalization;
-using Ionic.Zip;
 using UnityEngine.Networking;
-using UnityEngine.Networking.NetworkSystem;
 
 namespace MPGungeon
 {
 	public class Module : ETGModule
 	{
-
+		//seems like server and client might be seperate dll's in the HK mod.
 		public static readonly string MOD_NAME = "Gungeon Multiplayer";
 		public static readonly string VERSION = "1.0.0";
 		public static readonly string TEXT_COLOR = "#00FFFF";
-		NetworkClient client = new NetworkClient();
 		public override void Start()
 		{
 			try
 			{
 			
-				GameObject NetworkController = new GameObject("Network Manager");
-				NetworkController.AddComponent<MPNetworkManager>();
-
+				
 				ETGModConsole.Commands.AddGroup("mp", args =>
 				{
-					Log("Gungeon Multiplayer mod made by An3s :)");
+					Log("Gungeon Multiplayer mod made by An3s and Glorfindel (ScionOfMemory) :)");
 				});
 
-				ETGModConsole.Commands.GetGroup("mp").AddUnit("help", args =>
-				{
-					ETGModConsole.Log("You will be prompted when you must enter something in the console");
-				});
-
-				ETGModConsole.Commands.GetGroup("mp").AddUnit("serverport", this.GetInputServerPort);
-				ETGModConsole.Commands.GetGroup("mp").AddUnit("host", this.HostGame);
-				ETGModConsole.Commands.GetGroup("mp").AddUnit("join", this.JoinGame);
-				ETGModConsole.Commands.GetGroup("mp").AddUnit("test", this.Test);
+				
 			}
 			catch (Exception e)
 			{
 				ETGModConsole.Log("mod Broke heres why: " + e);
 			}
 			Log($"{MOD_NAME} v{VERSION} started successfully.", TEXT_COLOR);
-			Log("<color=#ff0000ff>Type in \"mp host\" to host a game and \"mp join\" to join a game</color>");
-		}
-
-        private void Test(string[] obj)
-        {
-			var message = new StringMessage("haha yes");
-			NetworkServer.SendToAll(1002,	message);
-		}
-
-        private void HostGame(string[] arg)
-        {
-			if(MPNetworkManager.isAtStartup == true)
-				MPNetworkManager.GetServerPort();
-		}
-
-        private void JoinGame(string[] arg)
-        {
-			Client client = new Client();
-			if (MPNetworkManager.isAtStartup == true)
-				client.CreateClient();
-		}
-
-        private void GetInputServerPort(string[] arg)
-		{
-			if (MPNetworkManager.isAtStartup == true)
-			{
-				Host host = new Host();
-				host.SetupServer();
-				host.SetupLocalClient();
-			}
 		}
 
 
