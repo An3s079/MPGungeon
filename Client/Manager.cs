47948c5e4e669a73bc7f3f7f9a3d94777554f736
An3s079 <68040208+An3s079@users.noreply.github.com>
2021-08-15 15:34:51 -1000

    Update Manager.cs

--- Client/Manager.cs
+++ Client/Manager.cs
@@ -15,21 +15,8 @@ namespace MPGungeon.Client
         public static void SpawnPlayer(int _id, string _username, string character, Vector3 _position)
         {
             GameObject _player;
-            if (_id == Client.instance.myId)
-            {
-                _player = Instantiate(BraveResources.Load("Player" + character, ".prefab") as GameObject, _position, Quaternion.identity);
-            }
-            else
-            {
-                _player = Instantiate(BraveResources.Load("Player" + character, ".prefab") as GameObject, _position, Quaternion.identity);
-            }
+  	    _player = Instantiate(BraveResources.Load("Player" + character, ".prefab") as GameObject, _position, Quaternion.identity);
 
-            //_player.GetComponent<PlayerManager>().id = _id;
-            //_player.GetComponent<PlayerManager>().username = _username;
-            FieldInfo myFieldInfo = typeof(PlayerController).GetField("m_activeActions",
-                BindingFlags.NonPublic | BindingFlags.Instance);
-
-            myFieldInfo.SetValue(_player.GetComponent<PlayerController>(), new GungeonActions());
             players.Add(_id, _player.GetComponent<PlayerController>());
         }
     }
