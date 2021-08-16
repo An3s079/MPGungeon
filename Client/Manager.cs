9fc988c41c4071c645e48ecdca85d452e190be5c
An3s079 <68040208+An3s079@users.noreply.github.com>
2021-08-15 16:44:17 -1000

    Update Manager.cs

--- Client/Manager.cs
+++ Client/Manager.cs
@@ -10,14 +10,14 @@ namespace MPGungeon.Client
 	class Manager : MonoBehaviour
 	{
 
-        public static Dictionary<int, PlayerController> players = new Dictionary<int, PlayerController>();
+        public static Dictionary<int, GameObject> players = new Dictionary<int, GameObject>();
 
         public static void SpawnPlayer(int _id, string _username, string character, Vector3 _position)
         {
-            GameObject _player;
-  	    _player = Instantiate(BraveResources.Load("Player" + character, ".prefab") as GameObject, _position, Quaternion.identity);
-
-            players.Add(_id, _player.GetComponent<PlayerController>());
-        }
+            GameObject _player = new GameObject();
+			ItemAPI.ItemBuilder.AddSpriteToObject("player2", "MpGungeon/Resources/RedSquare", _player);
+			Instantiate(_player, _position, Quaternion.identity);
+			players.Add(_id, _player);
+	}
     }
 }
